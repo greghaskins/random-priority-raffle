7a27c70e5e08db5910ac85ee1a7bd19383974aa0
Gregory Haskins <greg@greghaskins.com>
2019-07-27 19:00:58 -0400

    implement random drawing order

--- raffle.py
+++ raffle.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
 import random
-from typing import NewType, Mapping, Collection, Sequence
+from typing import NewType, Mapping, Collection, Iterable, Sequence
 
 Participant = NewType('Participant', str)
 Prize = NewType('Prize', str)
 
 
-def raffle(
-        prizes: Sequence[Prize],
-        entries: Collection[Participant],
-        preferences: Mapping[Participant, Sequence[Prize]],
-        random_source: random.Random = random.SystemRandom()) -> Mapping[Participant, Prize]:
+def raffle(prizes: Sequence[Prize],
+           entries: Collection[Participant],
+           preferences: Mapping[Participant, Sequence[Prize]],
+           random_source: random.Random = random.SystemRandom()
+           ) -> Mapping[Participant, Prize]:
 
     hat = list(entries)
     results = {}
@@ -21,3 +21,29 @@ def raffle(
         results[winner] = prize
 
     return results
+
+
+def draw_selection_order(
+        entries: Collection[Participant],
+        random_source: random.Random,
+) -> Iterable[Participant]:
+
+    selection_order = []
+    hat = list(entries)
+
+    while hat:
+        random_index = random_source.randrange(len(hat))
+        participant = hat.pop(random_index)
+        if participant not in selection_order:
+            selection_order.append(participant)
+
+    return selection_order
+
+
+def assign_prizes(
+        prizes: Sequence[Prize],
+        preferences: Mapping[Participant, Sequence[Prize]],
+        selection_order: Iterable[Participant],
+) -> Mapping[Participant, Prize]:
+
+    return {}
--- raffle_test.py
+++ raffle_test.py
@@ -1,4 +1,7 @@
+import random
+
 import raffle
+
 
 def test_trivial_case():
     prizes = ['foo']
@@ -8,3 +11,28 @@ def test_trivial_case():
     results = raffle.raffle(prizes, entries, preferences)
     assert results == {'alice': 'foo'}
 
+
+def test_selection_order_is_drawn_at_random():
+    entries = ['alice', 'bob', 'carol']
+
+    draw = raffle.draw_selection_order
+    assert draw(entries, random.Random(4)) == ['alice', 'carol', 'bob']
+    assert draw(entries, random.Random(999)) == ['carol', 'alice', 'bob']
+    assert draw(entries, random.Random(-8675309)) == ['bob', 'alice', 'carol']
+
+
+def test_selection_order_only_includes_each_participant_once():
+    entries = [
+        'alice',
+        'bob',
+        'alice',
+        'carol',
+        'bob',
+        'carol',
+        'alice',
+        'alice',
+    ]
+
+    results = list(raffle.draw_selection_order(entries, random.Random(42)))
+    assert len(results) == 3
+    assert set(results) == {'alice', 'carol', 'bob'}
